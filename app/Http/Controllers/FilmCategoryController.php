1e904a694fbb4a620f9705abb885c9d5050de8ce
davzamber <davidzb2@icloud.com>
2021-06-13 14:49:27 +0200

    subida

---
+++ app/Http/Controllers/FilmCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\film_category;
+use Illuminate\Http\Request;
+
+class FilmCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\film_category  $film_category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(film_category $film_category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\film_category  $film_category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(film_category $film_category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\film_category  $film_category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, film_category $film_category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\film_category  $film_category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(film_category $film_category)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_06_13_104334_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres', 100);
+            $table->string('apellidos', 100);
+            $table->string('correo', 100);
+            $table->foreignId('address_id');
+            $table->date('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
